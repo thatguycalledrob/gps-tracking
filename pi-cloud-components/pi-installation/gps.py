64a093dbe59150ea6571cbd9d10377e58012dc9b
Rob <robertzhowie@gmail.com>
2020-06-22 11:27:57 +0100

    move to a cron tab approach

--- pi-cloud-components/pi-installation/gps.py
+++ pi-cloud-components/pi-installation/gps.py
@@ -1,13 +1,14 @@
+#! /usr/bin/python3
 from typing import List
 from os.path import dirname, join, abspath
-from swagger_client import Coordinate, DefaultApi
+from swagger_client import Coordinate, DefaultApi, ApiClient
 from swagger_client.configuration import Configuration
 
 import datetime
 import json
 import pynmea2
+import multiprocessing
 import serial
-import swagger_client
 import sys
 import time
 
@@ -39,7 +40,6 @@ def read_gps(line):
 
 
 def setup_client() -> DefaultApi:
-
     # read the secret.json file right off
     with open(join(abspath(dirname(__file__)), 'secret.json'), mode='r', encoding='utf-8') as f:
         try:
@@ -58,18 +58,14 @@ def setup_client() -> DefaultApi:
     cl.host = url
     cl.api_key = {'X-API-Key': secret}
 
-    return swagger_client.DefaultApi(
-        swagger_client.ApiClient(cl)
-    )
+    return DefaultApi(ApiClient(cl))
 
 
-if __name__ == '__main__':
+def main() -> None:
     # reporting
-    port_read_rate = 0.01  # how often to read from the serial port
-    observation_interval = 10  # how often (in s) to take a reading
-    report_interval = 60  # how often (in s) to make an API call
-
-    counter = 0
+    PORT_READ_RATE = 0.01  # how often to read from the serial port
+    OBSERVATION_INTERVAL = 10  # how often (in s) to take a reading
+    REPORT_INTERVAL = 60  # how often (in s) to make an API call
 
     api_client = setup_client()
 
@@ -77,59 +73,78 @@ if __name__ == '__main__':
     # baudrate chosen from gps chip manual.
     port = serial.Serial('/dev/serial0', baudrate=9600, timeout=0.5)
 
-    while True:
-        counter += 1
-        print(f"running round: {counter}")
-        try:
+    try:
+        # Initialise message body
+        msgBody: List[Coordinate] = []
 
-            # Initialise message body
-            msgBody: List[Coordinate] = []
+        for c in range(int(REPORT_INTERVAL / OBSERVATION_INTERVAL)):
 
-            for c in range(int(report_interval / observation_interval)):
+            observations = []
+            for _ in range(int(OBSERVATION_INTERVAL * PORT_READ_RATE ** -1)):
+                time.sleep(PORT_READ_RATE)
+                try:
+                    if port.inWaiting():
+                        x = read_gps(port.readline())
+                        if x is not None:
+                            observations.append(x)
+                except Exception:
+                    # on IO failure, close and port with wait
+                    port.close()
+                    time.sleep(1)
+                    port.open()
 
-                observations = []
-                for _ in range(int(observation_interval * port_read_rate ** -1)):
-                    time.sleep(port_read_rate)
-                    try:
-                        if port.inWaiting():
-                            x = read_gps(port.readline())
-                            if x is not None:
-                                observations.append(x)
-                    except Exception as e:
-                        # on IO failure, close and port with wait
-                        port.close()
-                        time.sleep(1)
-                        port.open()
+            if observations:
+                out = [float(sum(o)) / len(o) for o in zip(*observations)]
 
-                if observations:
-                    out = [float(sum(o)) / len(o) for o in zip(*observations)]
-
-                    # Use the API struct to define the Coordinates
-                    msgBody.append(
-                        Coordinate(
-                            time=int(datetime.datetime.utcnow().timestamp()),
-                            lat=float("{:.5f}".format(out[0])),
-                            long=float("{:.5f}".format(out[1])),
-                            alt=float("{:.1f}".format(out[2])),
-                            sats=float("{:.1f}".format(out[3])),
-                            order=float(c)
-                        )
+                # Use the API struct to define the Coordinates
+                msgBody.append(
+                    Coordinate(
+                        time=int(datetime.datetime.utcnow().timestamp()),
+                        lat=float("{:.5f}".format(out[0])),
+                        long=float("{:.5f}".format(out[1])),
+                        alt=float("{:.1f}".format(out[2])),
+                        sats=float("{:.1f}".format(out[3])),
+                        order=float(c)
                     )
+                )
 
-                # reset the IO stream between observations
-                # this reduces frequency of locks and drops
-                port.close()
-                time.sleep(5)
-                port.open()
+            # reset the IO stream between observations
+            # this reduces frequency of locks and drops
+            port.close()
+            time.sleep(5)
+            port.open()
 
-            api_client.add_coordinates(msgBody)
+        api_client.add_coordinates(msgBody)
 
-        except Exception as e:
+    except Exception as e:
 
-            # ah... the issue with wrapping everything in a try catch!
-            if e == KeyboardInterrupt:
-                exit(0)
+        # ah... the issue with wrapping everything in a try catch!
+        if e == KeyboardInterrupt:
+            sys.exit(0)
 
-            # we can probably log this somewhere if we have issues
-            print("Critical runtime error!", json.dumps(e, default=str))
+        # we can probably log this somewhere if we have issues
+        print("Critical runtime error!", json.dumps(e, default=str))
+        pass
+
+
+if __name__ == '__main__':
+    # 1 min 30s timeout
+    # This main thread will be spawned on a cron schedule of 2 minuites
+    # it will read data for approx 60s, and then transit via a HTTP API.
+    # This leaves 30s for spinup and shutdown, plus 30s between the cron
+    # taks for the py interpreter to clean itself up.
+    TIMEOUT = 90
+
+    p = multiprocessing.Process(target=main, name="read")
+    p.start()
+
+    # obey a 90s timeout on the processing thread
+    p.join(TIMEOUT)
+    if p.is_alive():
+        try:
+            p.terminate()
+            p.join()
+        except Exception:
             pass
+
+    sys.exit(0)
